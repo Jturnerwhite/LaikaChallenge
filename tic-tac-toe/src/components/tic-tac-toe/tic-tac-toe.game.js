f53cfffc8c0af967f40d51601943c8958ead2fd7
jturnerwhite@gmail.com <jturnerwhite@gmail.com>
2021-08-21 14:38:04 -0500

    first pass at game logic

--- tic-tac-toe/src/components/tic-tac-toe/tic-tac-toe.game.js
+++ tic-tac-toe/src/components/tic-tac-toe/tic-tac-toe.game.js
@@ -4,20 +4,16 @@ import './tic-tac-toe.css';
 class TicTacToe extends Component {
     render() {
         const {tiles, update} = this.props;
-        console.log("tiles", tiles);
 
         // For our game, we will use "X" and "O" string values for representation for legibility
         // For now do state initialization here, but in the future this will be handled elsewhere
         let tileRows = [];
         tiles.forEach((row, yIndex) => {
-            console.log(row);
             row.forEach((value, xIndex) => {
                 let keyString = "y:" + yIndex + "|x:" + xIndex;
                 tileRows.push(<Tile key={keyString} yindex={yIndex} xindex={xIndex} entry={value} update={update}/>);
             });
         });
-
-        console.log(tileRows);
 
         return (
             <div className="ttt-board">
--- tic-tac-toe/src/pages/game.page.js
+++ tic-tac-toe/src/pages/game.page.js
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 import TicTacToe from '../components/tic-tac-toe/tic-tac-toe.game';
 
 class GamePage extends Component {
+    winValue = 3; // allows us to reuse game as "connect four" someday (?)
+
     state = {
         turn: "X",
         tiles: [
@@ -29,7 +31,7 @@ class GamePage extends Component {
         const {turn, tiles} = this.state;
 
         if(tiles[yIndex][xIndex] !== "") {
-            alert("That space is taken"); // ugly but temporary
+            this.renderAlert("That space is taken"); // ugly but temporary
         }
         else {
             const updatedTiles = Array.from(tiles);
@@ -44,8 +46,82 @@ class GamePage extends Component {
         }
     }
 
+    // hashing this out on the frontend, will relocate to backend after finishing
     assessState = () => {
-        const {turn, tiles} = this.state;
+        const {tiles} = this.state;
+        const winValue = this.winValue;
+
+        let winnerFound = false;
+        let totalFilled = 0;
+        let downRightDiagValue = 0;
+        for(var y = 0; y < tiles.length; y++) {
+            let totalRowValue = 0;
+            let totalColValue = 0;
+
+            for(var x = 0; x < tiles[y].length; x++) {
+                if(tiles[y][x] === "X") {
+                    totalFilled++;
+                    totalRowValue++;
+                }
+                else if(tiles[y][x] === "O") {
+                    totalFilled++;
+                    totalRowValue--;
+                }
+
+                // Unintuitively, this can simultaneously check for column wins.
+                if(tiles[x][y] === "X") {
+                    totalColValue++;
+                }
+                else if(tiles[x][y] === "O") {
+                    totalColValue--;
+                }
+
+                if(x === y) {
+                    if(tiles[x][y] === "X") {
+                        downRightDiagValue++;
+                    }
+                    else if(tiles[x][y] === "O") {
+                        downRightDiagValue--;
+                    }
+                }
+            }
+
+            if(totalRowValue === winValue || totalColValue === winValue) {
+                winnerFound = true;
+                this.renderAlert("X won");
+            }
+            else if(totalRowValue === -winValue || totalColValue === -winValue) {
+                winnerFound = true;
+                this.renderAlert("O won");
+            }
+        }
+
+        if(downRightDiagValue === winValue || downRightDiagValue === winValue) {
+            winnerFound = true;
+            this.renderAlert("X won");
+        }
+        else if(downRightDiagValue === -winValue || downRightDiagValue === -winValue) {
+            winnerFound = true;
+            this.renderAlert("O won");
+        }
+
+        // wanted to move on from this, couldn't think of rapid+easy+clever check for this
+        if(tiles[0][2] === tiles[1][1] && tiles[1][1] === tiles[2][0] && tiles[1][1] === "X") {
+            winnerFound = true;
+            this.renderAlert("X won");
+        }
+        if(tiles[0][2] === tiles[1][1] && tiles[1][1] === tiles[2][0] && tiles[1][1] === "O") {
+            winnerFound = true;
+            this.renderAlert("O won");
+        }
+
+        if(!winnerFound && totalFilled === (tiles.length * tiles[0].length)) {
+            this.renderAlert('TIE');
+        }
+    }
+
+    renderAlert = (message) => {
+        alert(message); // so I can refactor quickly later
     }
 
     render() {
