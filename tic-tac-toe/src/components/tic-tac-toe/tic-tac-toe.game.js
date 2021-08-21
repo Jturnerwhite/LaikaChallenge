356c87521d12b99d79dd9739039c240ef389c615
jturnerwhite@gmail.com <jturnerwhite@gmail.com>
2021-08-21 13:10:30 -0500

    Initial state setup and usage

--- tic-tac-toe/src/components/tic-tac-toe/tic-tac-toe.game.js
+++ tic-tac-toe/src/components/tic-tac-toe/tic-tac-toe.game.js
@@ -3,25 +3,29 @@ import './tic-tac-toe.css';
 
 class TicTacToe extends Component {
     render() {
+        const state = {
+            tiles: []
+        }
+
+        // For our game, we will use "X" and "O" string values for representation for legibility
+        // For now do state initialization here, but in the future this will be handled elsewhere
+        let tileElements = [];
+        for(var i = 0; i < 9; i++) {
+            state.tiles.push("");
+            tileElements.push(<Tile entry={""} />)
+        }
+
         return (
             <div class="ttt-board">
-                <TTTSpace/>
-                <TTTSpace/>
-                <TTTSpace/>
-                <TTTSpace/>
-                <TTTSpace/>
-                <TTTSpace/>
-                <TTTSpace/>
-                <TTTSpace/>
-                <TTTSpace/>
+                {tileElements}
             </div>
         );
     }
 }
 
-const TTTSpace = () => {
+const Tile = (props) => {
     return (
-        <button class="space-btn">click me</button>
+        <button>{props.entry}</button>
     );
 }
 
