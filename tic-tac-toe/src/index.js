20567e7014856a623ca87448bd4b05ab532e3230
jturnerwhite@gmail.com <jturnerwhite@gmail.com>
2021-08-21 12:37:18 -0500

    Basic routing added, breaks css but thats fine

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,19 +1,41 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 
 import LandingPage from './pages/landing.page';
+import GamePage from './pages/game.page';
 import './index.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <LandingPage/>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<Link to="/">Home</Link>
+				<Link to="/game">Game</Link>
+				<Switch>
+					<Route path="/game">
+						<GamePage/>
+					</Route>
+					<Route path="/">
+						<LandingPage/>
+					</Route>
+				</Switch>
+			</Router>
+		);
+	}
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <LandingPage/>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 ReactDOM.render(<App />, document.getElementById('root'))
---
+++ tic-tac-toe/src/pages/end.page.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class EndPage extends Component {
+    render() {
+        return (
+            <div class="end-page">
+                <h1>End Page</h1>
+            </div>
+        );
+    }
+}
+
+export default EndPage;
