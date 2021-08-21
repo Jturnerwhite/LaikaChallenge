8dd36bf8d7d58de86424e8f290e4d9ddd686c16b
jturnerwhite@gmail.com <jturnerwhite@gmail.com>
2021-08-21 13:20:58 -0500

    migrating state to the page-level

--- tic-tac-toe/src/components/tic-tac-toe/tic-tac-toe.game.js
+++ tic-tac-toe/src/components/tic-tac-toe/tic-tac-toe.game.js
@@ -3,20 +3,16 @@ import './tic-tac-toe.css';
 
 class TicTacToe extends Component {
     render() {
-        const state = {
-            tiles: []
-        }
+        const tiles = this.props.tiles;
 
         // For our game, we will use "X" and "O" string values for representation for legibility
         // For now do state initialization here, but in the future this will be handled elsewhere
-        let tileElements = [];
-        for(var i = 0; i < 9; i++) {
-            state.tiles.push("");
-            tileElements.push(<Tile entry={""} />)
-        }
+        let tileElements = tiles.map((tile, index) => {
+            return (<Tile key={index} entry={tile} />);
+        });
 
         return (
-            <div class="ttt-board">
+            <div className="ttt-board">
                 {tileElements}
             </div>
         );
--- tic-tac-toe/src/pages/game.page.js
+++ tic-tac-toe/src/pages/game.page.js
@@ -3,11 +3,29 @@ import React, {Component} from 'react';
 import TicTacToe from '../components/tic-tac-toe/tic-tac-toe.game';
 
 class GamePage extends Component {
+    state = {
+        tiles: []
+    }
+
+    constructor(props) {
+        super(props);
+
+        if(!this.state || this.state.tiles.length === 0) {
+            this.state = {
+                tiles: []
+            };
+
+            for(var i = 0; i < 9; i++) {
+                this.state.tiles.push("");
+            }
+        }
+    }
+
     render() {
         return (
-            <div class="game-page">
+            <div className="game-page">
                 <h1>Game Page</h1>
-                <TicTacToe/>
+                <TicTacToe tiles={this.state.tiles}/>
             </div>
         );
     }
